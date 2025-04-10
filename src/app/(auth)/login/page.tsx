a6bada34586f5e4ad76b139699a0f82500bb264c
GAFT <gabrielferrarisan@gmail.com>
2025-04-09 21:54:51 -0300

    inicial-confiurgation

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { PasswordInput } from "@/components/ui/password-input";
+import {
+  Fieldset,
+  Stack,
+  Field,
+  Input,
+  Button,
+  Center,
+  Separator,
+  Text,
+  Highlight,
+} from "@chakra-ui/react";
+import Link from "next/link";
+
+export default function Login() {
+  return (
+    <Center h="100vh">
+      <Fieldset.Root
+        size="lg"
+        maxW="md"
+        border="1px solid #fff"
+        p="1rem"
+        borderRadius="xl"
+      >
+        <Stack>
+          <Fieldset.Legend fontSize="2rem">Bem vindo de volta!</Fieldset.Legend>
+          <Fieldset.HelperText>Seja bem vindo a dev shop!</Fieldset.HelperText>
+        </Stack>
+
+        <Fieldset.Content>
+          <Field.Root>
+            <Field.Label>Name:</Field.Label>
+            <Input name="name" />
+          </Field.Root>
+
+          <Field.Root>
+            <Field.Label>Email address:</Field.Label>
+            <Input name="email" type="email" />
+          </Field.Root>
+
+          <Field.Root>
+            <Field.Label>Senha:</Field.Label>
+            <PasswordInput name="password" />
+          </Field.Root>
+
+          <Field.Root>
+            <Field.Label>Confirmar senha:</Field.Label>
+            <PasswordInput name="confirmPassword" />
+          </Field.Root>
+        </Fieldset.Content>
+
+        <Separator />
+
+        <Button
+          type="submit"
+          alignSelf="flex-start"
+          w="100%"
+          mx="auto"
+          borderRadius="30px"
+        >
+          Submit
+        </Button>
+
+        <Separator />
+
+        <Text>
+          Nâo tem uma conta?{" "}
+          <Link href="/register">
+            <Highlight query="Cadastre-se" styles={{ color: "#00b3b9" }}>
+              Cadastre-se
+            </Highlight>
+          </Link>
+        </Text>
+      </Fieldset.Root>
+    </Center>
+  );
+}
---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,80 @@
+"use client";
+import { PasswordInput } from "@/components/ui/password-input";
+import {
+  Fieldset,
+  Stack,
+  Field,
+  Input,
+  Button,
+  Center,
+  Separator,
+  Text,
+  Highlight,
+} from "@chakra-ui/react";
+import Link from "next/link";
+
+export default function Register() {
+  return (
+    <Center h="100vh">
+      <Fieldset.Root
+        size="lg"
+        maxW="md"
+        border="1px solid #fff"
+        p="1rem"
+        borderRadius="xl"
+      >
+        <Stack>
+          <Fieldset.Legend fontSize="2rem">
+            Bem vindo ao Dev Shop!
+          </Fieldset.Legend>
+          <Fieldset.HelperText>É um prazer ter você aqui!</Fieldset.HelperText>
+        </Stack>
+
+        <Fieldset.Content>
+          <Field.Root>
+            <Field.Label>Name</Field.Label>
+            <Input name="name" />
+          </Field.Root>
+
+          <Field.Root>
+            <Field.Label>Email address</Field.Label>
+            <Input name="email" type="email" />
+          </Field.Root>
+
+          <Field.Root>
+            <Field.Label>Senha:</Field.Label>
+            <PasswordInput name="password" />
+          </Field.Root>
+
+          <Field.Root>
+            <Field.Label>Confirmar senha:</Field.Label>
+            <PasswordInput name="confirmPassword" />
+          </Field.Root>
+        </Fieldset.Content>
+
+        <Separator />
+
+        <Button
+          type="submit"
+          alignSelf="flex-start"
+          w="100%"
+          mx="auto"
+          borderRadius="30px"
+        >
+          Submit
+        </Button>
+
+        <Separator />
+
+        <Text>
+          Já tem conta?{" "}
+          <Link href="/login">
+            <Highlight query="Entrar" styles={{ color: "#00b3b9" }}>
+              Entrar
+            </Highlight>
+          </Link>
+        </Text>
+      </Fieldset.Root>
+    </Center>
+  );
+}
